27fd0b616dd23274019d1f3019fcc29063cea84d
ThinkerRing <thinkerring2020@gmai.com>
2022-02-27 04:44:37 +0900

    ...

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 'use state'
 
+const today = document.querySelector('.date')
+const pm10 = document.querySelector('.pm10')
+const pm25 = document.querySelector('.pm25')
+const 황사 = document.querySelector('.황사')
+
+// 위치 묻기
+// addEventListener(load) {}
+// 날짜 가져오기
+let year = new Date().getFullYear()
+let month = new Date().getMonth()
+let date = new Date().getDate()
+
+today.innerHTML=`${year}. ${month + 1}. ${date}`
+
 // WGS84 위도, 경도 값 출력 
 function onGeoOk(position) {
   const lat = position.coords.latitude
